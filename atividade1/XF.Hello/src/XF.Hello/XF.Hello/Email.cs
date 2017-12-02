dca4a870379a6b31f9dad42df001aa151e4d9a02
Reginaldo Alves de Barros <R21Regis@hotmail.com>
2017-12-02 20:58:05 -0200

    execicio 1

---
+++ atividade1/XF.Hello/src/XF.Hello/XF.Hello/Email.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace XF.Hello
+{
+    public class Email: INotifyPropertyChanged
+    {
+        private string _conta;
+
+        public string Conta
+        {
+            get
+            {
+                return _conta;
+            }
+            set
+            {
+                _conta = value;
+                EventPropertyChanged();
+            }
+        }
+
+        private bool _aceitaEnvio;
+        public bool AceitaEnvio
+        {
+            get
+            {
+                return _aceitaEnvio;
+            }
+            set
+            {
+                _aceitaEnvio = value;
+
+                if (!_aceitaEnvio)
+                    Conta = string.Empty;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void EventPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ atividade1/XF.Hello/src/XF.Hello/XF.Hello/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XF.Hello
+{
+    public partial class MainPage : ContentPage
+    {
+        Email Email;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Email = new Email();
+        }
+
+        private void EnviarEmail_Clicked(object sender, EventArgs e)
+        {
+            if (Email.AceitaEnvio && !string.IsNullOrEmpty(Email.Conta))
+            {
+                DisplayAlert("Mensagem", $"E-mail enviado para {Email.Conta}", "OK");
+            }
+            else
+            {
+                DisplayAlert("Mensagem", $"E-mail não autorizado", "OK");  
+            }
+       
+        }
+
+        private void Configuracao_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ConfigPage() { BindingContext = Email });
+        }
+    }
+}
